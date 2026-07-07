0a5e5ef15fc86a61eb448296fa1c4e3f93e59b93
juanjsales <juangomes.sales@gmail.com>
2026-07-06 22:40:57 -0300

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
-import topper1 from './assets/05XwpYCPnqvJ.jpg';
-import topper2 from './assets/pvwqMwgFp90y.jpg';
-import topper3 from './assets/KRYzzevQhcxo.jpg';
-import topper4 from './assets/1haMoyCFDXay.jpg';
-import topper5 from './assets/uoe1vNqkYbnJ.jpg';
-import topper6 from './assets/7ChSyaRK0961.jpeg';
+import topper1 from './assets/topper1.jpeg';
+import topper2 from './assets/topper2.jpeg';
+import topper3 from './assets/topper3.jpeg';
+import topper4 from './assets/topper4.jpeg';
+import topper5 from './assets/topper5.jpeg';
+import topper6 from './assets/05XwpYCPnqvJ.jpg';
 
 export const topperGallery = [
    { id: 1, image: topper1, title: 'Topos Personalizados 3D', description: 'Dê profundidade e magia ao seu bolo com topos 3D cheios de detalhes e camadas.' },
